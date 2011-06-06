svn2github/resourcespace

commit a0fd2482700ef8db7ab05f9b51805b2867c5bd0d

Restructured metadata field entry code to separate out each field type, making the code cleaner and adding new metadata types easier. Added a CKeditor metadata field type.
git-svn-id: http://svn.resourcespace.org/svn/resourcespace@2773 c08608d7-6e46-0410-86ca-f2a6f1370df5

pages/edit_fields/7.php
@@ -1,4 +1,6 @@
-<?php
+<?php /* -------- Category tree ---------------- */ 
+
+$options=$fields[$n]["options"];
 
 global $lang,$baseurl,$css_reload_key,$category_tree_show_status_window;
 

commit 0cf1eb1e1d9777f1b83d77efcaac393d7cd3804a

category trees broken in r2773. Attempting to fix.
git-svn-id: http://svn.resourcespace.org/svn/resourcespace@2780 c08608d7-6e46-0410-86ca-f2a6f1370df5

include/search_functions.php
@@ -1020,7 +1020,7 @@ function render_search_field($field,$value="",$autoupdate,$class="stdwidth",$for
 		else
 			{
 			# For advanced search and elsewhere, include the category tree.
-			include "category_tree.php";
+			include "../pages/edit_fields/7.php";
 			}
 		break;
 		}

pages/ajax/category_tree_popup.php
@@ -23,6 +23,6 @@ $options=$fdata["options"];
 # Show the category tree
 $category_tree_open=true;
 $treeonly=true;
-include "../../include/category_tree.php";
+include "../edit_fields/7.php";
 
 ?>

pages/edit_fields/7.php
@@ -1,6 +1,8 @@
 <?php /* -------- Category tree ---------------- */ 
 
-$options=$fields[$n]["options"];
+if (isset($n) && isset($fields)){
+	$options=$fields[$n]["options"];
+}
 
 global $lang,$baseurl,$css_reload_key,$category_tree_show_status_window;
 