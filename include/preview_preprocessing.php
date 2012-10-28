svn2github/resourcespace

commit 9827f68e56486cb3317e8ca2420cdf3dc9dda54f

Introduced $imagemagick_colorspace config for newer ImageMagick versions.
git-svn-id: http://svn.resourcespace.org/svn/resourcespace@3869 c08608d7-6e46-0410-86ca-f2a6f1370df5

include/preview_preprocessing.php
@@ -571,7 +571,7 @@ if ((!isset($newfile)) && (!in_array($extension, $ffmpeg_audio_extensions)))
     $prefix="";
 
 	# Preserve colour profiles?    
-	$profile="+profile icc -colorspace RGB"; # By default, strip the colour profiles ('+' is remove the profile, confusingly)
+	$profile="+profile icc -colorspace ".$imagemagick_colorspace; # By default, strip the colour profiles ('+' is remove the profile, confusingly)
     if ($imagemagick_preserve_profiles) {$profile="";}
     
     # CR2 files need a cr2: prefix
@@ -609,7 +609,7 @@ if ((!isset($newfile)) && (!in_array($extension, $ffmpeg_audio_extensions)))
 			$eps_density_x = $eps_data_x / $eps_bbox_x * 72;
 			$eps_density_y = $eps_data_y / $eps_bbox_y * 72;
 			$eps_target=get_resource_path($ref,true,"",false,"miff");
-			$nfcommand = $convert_fullpath . ' -compress zip -colorspace RGB -quality 100 -density ' . sprintf("%.1f", $eps_density_x ). 'x' . sprintf("%.1f", $eps_density_y) . ' ' . escapeshellarg($file) . '[0] ' . escapeshellarg($eps_target);
+			$nfcommand = $convert_fullpath . ' -compress zip -colorspace '.$imagemagick_colorspace.' -quality 100 -density ' . sprintf("%.1f", $eps_density_x ). 'x' . sprintf("%.1f", $eps_density_y) . ' ' . escapeshellarg($file) . '[0] ' . escapeshellarg($eps_target);
 			run_command($nfcommand);
 			if (file_exists($eps_target))
 			{

include/config.default.php
@@ -268,6 +268,13 @@ $filename_field=51;
 $imagemagick_preserve_profiles=false;
 $imagemagick_quality=90; # JPEG quality (0=worst quality/lowest filesize, 100=best quality/highest filesize)
 
+# Colorspace usage
+# Use "RGB" for ImageMagick versions before 6.7.6-4
+# Use "RGB" for GraphicsMagick
+# Use "sRGB" for ImageMagick version 6.7.6-4 and newer
+$imagemagick_colorspace="RGB";
+
+
 # To use the Ghostscript command -dUseCIEColor or not (generally true but added in some cases where scripts might want to turn it off).
 $dUseCIEColor=true;
 
@@ -305,7 +312,7 @@ $ffmpeg_preview_min_width=32;
 $ffmpeg_preview_min_height=18;
 $ffmpeg_preview_max_width=480;
 $ffmpeg_preview_max_height=270;
-$ffmpeg_preview_options="-f flv -ar 22050 -b 650k -ab 32 -ac 1";
+$ffmpeg_preview_options="-f flv -ar 22050 -b 650k -ab 32k -ac 1";
 
 # If uploaded file is FLV, should we transcode it anyway?
 $ffmpeg_preview_force=false;
@@ -1835,6 +1842,7 @@ $search_public_collections_ref=true;
 # $calibre_path="/usr/bin";
 # Files with these extensions will be passed to calibre (if enabled above) for conversion to PDF and auto thumb-preview generation.
 $calibre_extensions=array("epub","mobi","lrf","pdb","chm","cbr","cbz");
+
 
 
 

include/image_processing.php
@@ -752,7 +752,7 @@ function create_previews($ref,$thumbonly=false,$extension="jpg",$previewonly=fal
 
 function create_previews_using_im($ref,$thumbonly=false,$extension="jpg",$previewonly=false,$previewbased=false,$alternative=-1)
 	{
-	global $imagemagick_path,$imagemagick_preserve_profiles,$imagemagick_quality;
+	global $imagemagick_path,$imagemagick_preserve_profiles,$imagemagick_quality,$imagemagick_colorspace;
 
 	$icc_transform_complete=false;
 	debug("create_previews_using_im(ref=$ref,thumbonly=$thumbonly,extension=$extension,previewonly=$previewonly,previewbased=$previewbased,alternative=$alternative)");
@@ -889,7 +889,7 @@ function create_previews_using_im($ref,$thumbonly=false,$extension="jpg",$previe
 				} else {
 					// use existing strategy for color profiles
 					# Preserve colour profiles? (omit for smaller sizes)   
-					$profile="+profile \"*\" -colorspace RGB"; # By default, strip the colour profiles ('+' is remove the profile, confusingly)
+					$profile="+profile \"*\" -colorspace ".$imagemagick_colorspace; # By default, strip the colour profiles ('+' is remove the profile, confusingly)
 					if ($imagemagick_preserve_profiles && $id!="thm" && $id!="col" && $id!="pre" && $id!="scr") {$profile="";}
 				}
 

pages/ajax/contactsheet.php
@@ -317,7 +317,7 @@ for ($n=0;$n<count($result);$n++){
         $convert_fullpath = get_utility_path("im-convert");
         if ($convert_fullpath==false) {exit("Could not find ImageMagick 'convert' utility at location '$imagemagick_path'");}
 
-        $command = $convert_fullpath . " -resize ".$contact_sheet_preview_size." -quality 90 -colorspace RGB \"".get_temp_dir() . "/contactsheetrip.jpg\" \"".get_temp_dir() . "/contactsheet.jpg\"";
+        $command = $convert_fullpath . " -resize ".$contact_sheet_preview_size." -quality 90 -colorspace ".$imagemagick_colorspace." \"".get_temp_dir() . "/contactsheetrip.jpg\" \"".get_temp_dir() . "/contactsheet.jpg\"";
 		run_command($command);
 		exit();
 		}

plugins/annotate/include/general.php
@@ -36,7 +36,7 @@ function create_annotated_pdf($ref,$is_collection=false,$size="letter",$cleanup=
 	# This leaves the pdfs and jpg previews in filestore/annotate so that they can be grabbed later.
 	# $cleanup will result in a slightly different path that is not cleaned up afterwards.
 	
-	global $contact_sheet_preview_size,$annotate_pdf_output_only_annotated,$lang,$userfullname,$view_title_field,$baseurl,$imagemagick_path,$ghostscript_path,$previewpage,$storagedir,$storageurl,$annotate_font,$access,$k;
+	global $contact_sheet_preview_size,$annotate_pdf_output_only_annotated,$lang,$userfullname,$view_title_field,$baseurl,$imagemagick_path,$imagemagick_colorspace,$ghostscript_path,$previewpage,$storagedir,$storageurl,$annotate_font,$access,$k;
 	$date= date("m-d-Y h:i a");
 	
 	include_once($storagedir.'/../include/search_functions.php');
@@ -224,7 +224,7 @@ function create_annotated_pdf($ref,$is_collection=false,$size="letter",$cleanup=
 		if (!file_exists($command)) {$command=$imagemagick_path . "/convert";}
 		if (!file_exists($command)) {exit("Could not find ImageMagick 'convert' utility at location '$command'");}	
 		
-		$command.= " -resize $contact_sheet_preview_size -quality 90 -colorspace RGB " . escapeshellarg($jpgstoragepath) ." " . escapeshellarg($jpgstoragepath);
+		$command.= " -resize $contact_sheet_preview_size -quality 90 -colorspace ".$imagemagick_colorspace." " . escapeshellarg($jpgstoragepath) ." " . escapeshellarg($jpgstoragepath);
 		run_command($command);
 		return true;
 		}

commit 77235d5edc7bd44fbb97e58850546f5eeaafc8b8

Added $imagemagick_colorspace to globals. Fixes bug introduced in r3869.
git-svn-id: http://svn.resourcespace.org/svn/resourcespace@3871 c08608d7-6e46-0410-86ca-f2a6f1370df5

include/preview_preprocessing.php
@@ -5,7 +5,7 @@
 # for example types that use GhostScript or FFmpeg.
 #
 
-global $imagemagick_path, $imagemagick_preserve_profiles, $imagemagick_quality, $ghostscript_path, $pdf_pages, $antiword_path, $unoconv_path, $pdf_dynamic_rip, $ffmpeg_audio_extensions, $ffmpeg_audio_params, $qlpreview_path,$ffmpeg_supported_extensions, $qlpreview_exclude_extensions;
+global $imagemagick_path, $imagemagick_preserve_profiles, $imagemagick_quality, $imagemagick_colorspace, $ghostscript_path, $pdf_pages, $antiword_path, $unoconv_path, $pdf_dynamic_rip, $ffmpeg_audio_extensions, $ffmpeg_audio_params, $qlpreview_path,$ffmpeg_supported_extensions, $qlpreview_exclude_extensions;
 global $dUseCIEColor;
 
 # Locate utilities