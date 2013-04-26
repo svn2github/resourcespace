svn2github/resourcespace

commit 3cc3d2521d4d7c25e7cdaab3bf2f6911d936fc7a

minor refactoring of ICC profile handling code to allow passing arbitrary files instead of specifying a resource ID
git-svn-id: http://svn.resourcespace.org/svn/resourcespace@4260 c08608d7-6e46-0410-86ca-f2a6f1370df5

include/image_processing.php
@@ -1597,9 +1597,20 @@ function AutoRotateImage ($src_image){
 		return false;
 	}
 }
+
+function extract_icc_profile ($ref,$extension){
+	// this is provided for compatibility. However, we are now going to rely on the caller to tell us the
+	// path of the file. extract_icc() is where the real work will happen.
+	$infile=get_resource_path($ref,true,"",true,$extension);	
+	if (extract_icc($infile)){
+		return true;
+	} else {
+		return false;
+	}
+}
 	
 
-function extract_icc_profile($ref,$extension) {
+function extract_icc($infile) {
    global $config_windows;
 
    # Locate imagemagick, or fail this if it isn't installed
@@ -1608,8 +1619,11 @@ function extract_icc_profile($ref,$extension) {
 
    if ($config_windows){ $stderrclause = ''; } else { $stderrclause = '2>&1'; }
 
-   $infile=get_resource_path($ref,true,"",true,$extension);	
-   $outfile=get_resource_path($ref,true,"",false,$extension.".icc");
+   //$outfile=get_resource_path($ref,true,"",false,$extension.".icc");
+   //new, more flexible approach: we will just create a file for anything the caller hands to us.
+   //this makes things work with alternatives, the deepzoom plugin, etc.
+   $path_parts = pathinfo($infile);
+   $outfile = $path_parts['dirname'] . '/' . $path_parts['filename'] . '.icc';
    
    if (file_exists($outfile)){
       // extracted profile already existed. We'll remove it and start over

commit 0f9f7157b4f5965a65db5be5736bf016024b91f9

Fixed a bug introduced in r4260 that had rendered icc extraction ineffective
git-svn-id: http://svn.resourcespace.org/svn/resourcespace@4427 c08608d7-6e46-0410-86ca-f2a6f1370df5

include/image_processing.php
@@ -1647,7 +1647,7 @@ function extract_icc($infile) {
    //new, more flexible approach: we will just create a file for anything the caller hands to us.
    //this makes things work with alternatives, the deepzoom plugin, etc.
    $path_parts = pathinfo($infile);
-   $outfile = $path_parts['dirname'] . '/' . $path_parts['filename'] . '.icc';
+   $outfile = $path_parts['dirname'] . '/' . $path_parts['filename'] .'.'. $path_parts['extension'] .'.icc';
    
    if (file_exists($outfile)){
       // extracted profile already existed. We'll remove it and start over