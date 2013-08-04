svn2github/resourcespace

commit 6c08bcb22178e2ab4bceddf23602b03c457f1ce7

CHG: Cleaned up edit.php a bit, moved lots of things into separate functions. Also merged multilingual_fields.php back in there; it would be nicer to move most of this into a edit_fields.php include instead. However, due to the edit_fields/ pages, I did not do that just yet.
git-svn-id: http://svn.resourcespace.org/svn/resourcespace@4736 c08608d7-6e46-0410-86ca-f2a6f1370df5

pages/edit_fields/4.php
@@ -4,7 +4,7 @@
 $dy="";
 $dm=$dd=$dh=$di=-1;
 
-if (($ref<0 || $value=="") && $reset_date_upload_template && $reset_date_field==$fields[$n]["ref"])
+if (($ref<0 || $value=="") && $reset_date_upload_template && $reset_date_field==$field["ref"])
 	{
 	# Upload template: always reset to today's date (if configured).
 	$dy=date("Y");$dm=intval(date("m"));$dd=intval(date("d"));
@@ -32,31 +32,31 @@ elseif ($value!="")
     }    
 ?>
 <select name="<?php echo $name?>-d"
-<?php if ($edit_autosave) {?>onChange="AutoSave('<?php echo $fields[$n]["ref"]?>');"<?php } ?>
+<?php if ($edit_autosave) {?>onChange="AutoSave('<?php echo $field["ref"]?>');"<?php } ?>
 ><option value=""><?php echo $lang["day"]?></option>
 <?php for ($m=1;$m<=31;$m++) {?><option <?php if($m==$dd){echo " selected";}?>><?php echo sprintf("%02d",$m)?></option><?php } ?>
 </select>
     
 <select name="<?php echo $name?>-m"
-<?php if ($edit_autosave) {?>onChange="AutoSave('<?php echo $fields[$n]["ref"]?>');"<?php } ?>
+<?php if ($edit_autosave) {?>onChange="AutoSave('<?php echo $field["ref"]?>');"<?php } ?>
 ><option value=""><?php echo $lang["month"]?></option>
 <?php for ($m=1;$m<=12;$m++) {?><option <?php if($m==$dm){echo " selected";}?> value="<?php echo sprintf("%02d",$m)?>"><?php echo $lang["months"][$m-1]?></option><?php } ?>
 </select>
    
-<input type=text size=5 name="<?php echo $name?>-y" value="<?php echo $dy?>" <?php if ($edit_autosave) {?>onChange="AutoSave('<?php echo $fields[$n]["ref"]?>');"<?php } ?>>
+<input type=text size=5 name="<?php echo $name?>-y" value="<?php echo $dy?>" <?php if ($edit_autosave) {?>onChange="AutoSave('<?php echo $field["ref"]?>');"<?php } ?>>
 
-<?php if ($fields[$n]["type"]!=10) { ?>
+<?php if ($field["type"]!=10) { ?>
 <!-- Time (optional) -->
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 
 <select name="<?php echo $name?>-h"
-<?php if ($edit_autosave) {?>onChange="AutoSave('<?php echo $fields[$n]["ref"]?>');"<?php } ?>
+<?php if ($edit_autosave) {?>onChange="AutoSave('<?php echo $field["ref"]?>');"<?php } ?>
 ><option value=""><?php echo $lang["hour-abbreviated"]?></option>
 <?php for ($m=0;$m<=23;$m++) {?><option <?php if($m==$dh){echo " selected";}?>><?php echo sprintf("%02d",$m)?></option><?php } ?>
 </select>
 
 <select name="<?php echo $name?>-i"
-<?php if ($edit_autosave) {?>onChange="AutoSave('<?php echo $fields[$n]["ref"]?>');"<?php } ?>
+<?php if ($edit_autosave) {?>onChange="AutoSave('<?php echo $field["ref"]?>');"<?php } ?>
 ><option value=""><?php echo $lang["minute-abbreviated"]?></option>
 <?php for ($m=0;$m<=59;$m++) {?><option <?php if($m==$di){echo " selected";}?>><?php echo sprintf("%02d",$m)?></option><?php } ?>
 </select>

include/multilingual_fields.php
@@ -1,39 +1,0 @@
-<?php
-# multilingual_fields.php
-# Allows language alternatives to be entered for free text metadata fields.
-?>
-<p><a href="#" class="OptionToggle" onClick="l=document.getElementById('LanguageEntry_<?php echo $n?>');if (l.style.display=='block') {l.style.display='none';this.innerHTML='<?php echo $lang["showtranslations"]?>';} else {l.style.display='block';this.innerHTML='<?php echo $lang["hidetranslations"]?>';} return false;"><?php echo $lang["showtranslations"]?></a></p>
-
-<table class="OptionTable" style="display:none;" id="LanguageEntry_<?php echo $n?>">
-<?php
-
-reset ($languages);
-foreach ($languages as $langkey => $langname)
-	{
-	if ($language!=$langkey)
-		{
-		if (array_key_exists($langkey,$translations)) {$transval=$translations[$langkey];} else {$transval="";}
-		?>
-		<tr>
-		<td nowrap valign="top"><?php echo htmlspecialchars($langname)?>&nbsp;&nbsp;</td>
-
-		<?php
-		if ($fields[$n]["type"]==0)
-			{
-			?>
-			<td><input type="text" class="stdwidth" name="multilingual_<?php echo $n?>_<?php echo $langkey?>" value="<?php echo htmlspecialchars($transval)?>"></td>
-			<?php
-			}
-		else
-			{
-			?>
-			<td><textarea rows=6 cols=50 name="multilingual_<?php echo $n?>_<?php echo $langkey?>"><?php echo htmlspecialchars($transval)?></textarea></td>
-			<?php
-			}
-		?>
-		</tr>
-		<?php	
-		}
-	}
-?>
-</table>

pages/edit_fields/0.php
@@ -1,4 +1,4 @@
 <?php /* -------- Text Box (single line) ---------- */ ?>
 <input class="stdwidth" type=text name="<?php echo $name?>" id="<?php echo $name?>" value="<?php echo htmlspecialchars($value)?>" <?php echo $help_js; ?>
-<?php if ($edit_autosave) {?>onChange="AutoSave('<?php echo $fields[$n]["ref"] ?>');"<?php } ?>
+<?php if ($edit_autosave) {?>onChange="AutoSave('<?php echo $field["ref"] ?>');"<?php } ?>
 >

pages/edit_fields/1.php
@@ -1,4 +1,4 @@
 <?php /* -------- Text Box (multi-line) ----------- */ ?>
 <textarea class="stdwidth" rows=6 cols=50 name="<?php echo $name?>" id="<?php echo $name?>" <?php echo $help_js; ?>
-<?php if ($edit_autosave) {?>onChange="AutoSave('<?php echo $fields[$n]["ref"] ?>');"<?php } ?>
+<?php if ($edit_autosave) {?>onChange="AutoSave('<?php echo $field["ref"] ?>');"<?php } ?>
 ><?php echo htmlspecialchars($value)?></textarea>

pages/edit_fields/11.php
@@ -1,7 +1,7 @@
 <?php /* -------- Category tree ---------------- */ 
 
-if (isset($n) && isset($fields)){
-	$options=$fields[$n]["options"];
+if (isset($field)){
+	$options=$field["options"];
 }
 
 global $lang,$baseurl,$css_reload_key,$category_tree_show_status_window;
@@ -59,7 +59,7 @@ for ($t=0;$t<count($class);$t++)
 		}
 	}
 */
-$tree=sql_query("select * from dynamic_tree_node where resource_type_field='" . $fields[$n]["ref"] . "'");
+$tree=sql_query("select * from dynamic_tree_node where resource_type_field='" . $field["ref"] . "'");
 
 foreach ($tree as $node)
 	{

pages/edit_fields/2.php
@@ -3,7 +3,7 @@
 if(!hook("customchkboxes")):
 
 # Translate all options
-$options=trim_array(explode(",",$fields[$n]["options"]));
+$options=trim_array(explode(",",$field["options"]));
 $option_trans=array();
 $option_trans_simple=array();
 for ($m=0;$m<count($options);$m++)
@@ -44,12 +44,12 @@ if ($checkbox_ordered_vertically)
 				$option=$options[$o];
 				$trans=$option_trans[$option];
 
-				$name=$fields[$n]["ref"] . "_" . md5($option);
+				$name=$field["ref"] . "_" . md5($option);
 				if ($option!="")
 					{
 					?>
 					<td width="1"><input type="checkbox" name="<?php echo $name?>" value="yes" <?php if (in_array($option,$set)) {?>checked<?php } ?> 
-					<?php if ($edit_autosave) {?>onChange="AutoSave('<?php echo $fields[$n]["ref"] ?>');"<?php } ?>
+					<?php if ($edit_autosave) {?>onChange="AutoSave('<?php echo $field["ref"] ?>');"<?php } ?>
 					/></td><td><?php echo htmlspecialchars($trans)?>&nbsp;</td>
 					<?php
 					}
@@ -69,11 +69,11 @@ else
 
 	foreach ($option_trans as $option=>$trans)
 		{
-		$name=$fields[$n]["ref"] . "_" . md5($option);
+		$name=$field["ref"] . "_" . md5($option);
 		$wrap++;if ($wrap>$cols) {$wrap=1;?></tr><tr><?php }
 		?>
 		<td width="1"><input type="checkbox" name="<?php echo $name?>" value="yes" <?php if (in_array($option,$set)) {?>checked<?php } ?>
-		<?php if ($edit_autosave) {?>onChange="AutoSave('<?php echo $fields[$n]["ref"] ?>');"<?php } ?>
+		<?php if ($edit_autosave) {?>onChange="AutoSave('<?php echo $field["ref"] ?>');"<?php } ?>
 		 /></td><td><?php echo htmlspecialchars($trans)?>&nbsp;</td>
 		<?php
 		}

pages/edit_fields/3.php
@@ -1,7 +1,7 @@
 <?php /* -------- Drop down list ------------------ */ 
 
 # Translate all options
-$options=trim_array(explode(",",$fields[$n]["options"]));
+$options=trim_array(explode(",",$field["options"]));
 
 $adjusted_dropdownoptions=hook("adjustdropdownoptions");
 if ($adjusted_dropdownoptions){$options=$adjusted_dropdownoptions;}
@@ -16,7 +16,7 @@ if ($auto_order_checkbox) {asort($option_trans);}
 if (substr($value,0,1) == ',') { $value = substr($value,1); }	// strip the leading comma if it exists	
 
 ?><select class="stdwidth" name="<?php echo $name?>" id="<?php echo $name?>" <?php echo $help_js; ?>
-<?php if ($edit_autosave) {?>onChange="AutoSave('<?php echo $fields[$n]["ref"] ?>');"<?php } ?>
+<?php if ($edit_autosave) {?>onChange="AutoSave('<?php echo $field["ref"] ?>');"<?php } ?>
 >
 <option value=""></option>
 <?php

commit 075e627ef78a3a1c0dd6322faebed34f1a9b5ed3

BUG: Fixed missing variables I introduced with r4736.
git-svn-id: http://svn.resourcespace.org/svn/resourcespace@4738 c08608d7-6e46-0410-86ca-f2a6f1370df5

pages/edit_fields/4.php
@@ -1,5 +1,7 @@
 <?php /* -------- Date ---------------------------- */ 
 
+global $reset_date_upload_template, $reset_date_field;
+
 # Start with a null date
 $dy="";
 $dm=$dd=$dh=$di=-1;