0660a6eb8760d3f950c406f23f1ab485f02ccb37
adoerfler <adoerfler@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2014-03-19 11:56:01 +0000

    ADD plugin user_preferences that allows users to change their username, full name, and email address.
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@5388 c08608d7-6e46-0410-86ca-f2a6f1370df5

---
+++ plugins/user_preferences/languages/en.php
@@ -0,0 +1,7 @@
+<?php
+# English
+
+$lang['user_preferences_configuration'] = 'Configuration for the user preferences';
+$lang['user_preferences_change_username'] = 'Allow changing the account name';
+$lang['user_preferences_change_email'] = 'Allow changing the E-mail address';
+$lang['user_preferences_change_name'] = 'Allow changing the user\'s name';
