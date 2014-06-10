b0798480acab3c563da725a1dcd77d248637b4ec
acota <acota@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2014-06-10 11:27:55 +0000

    FIX accessibility enhancement -removed h2 tag with no content because it will confuse screen readers
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@5532 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/themes.php
+++ pages/themes.php
@@ -625,7 +625,6 @@ if ($header=="" && !isset($themes[0]))
 <?php if (!checkperm("b") && $enable_public_collections) { ?>
 <div class="clearerleft"> </div>
 <div class="BasicsBox">
-	<h2>&nbsp;</h2>
     <h2><?php echo $lang["findpubliccollection"]?></h2>
     <p class="tight"><?php echo text("findpublic")?></p>
     <p><a href="<?php echo $baseurl_short?>pages/collection_public.php" onClick="return CentralSpaceLoad(this,true);"><?php echo $lang["findpubliccollection"]?>&nbsp;&gt;</a></p>
