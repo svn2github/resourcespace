52460ba431c43b3b39f853ccdcf4eae820e1cbad
jethro <jethro@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2014-06-26 09:22:45 +0000

    CHG Tidied login code to improve readability
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@5575 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- login.php
+++ login.php
@@ -14,12 +14,12 @@ $language=getval("language","");
 # Check the provided language is valid (XSS vuln. fix)
 if (!empty($language) && !array_key_exists($language,$languages))
 	{
-        exit("Invalid language");
-        }
+    	exit("Invalid language");
+    }
         
 # process log in
 $error=getval("error","");
-if ($error!="") $error=$lang[$error];
+if ($error!="") {$error=$lang[$error]};
 
 # Auto logged out? Set error message.
 if (getval("auto","")!="") {$error=str_replace("30",$session_length,$lang["sessionexpired"]);}
@@ -27,60 +27,60 @@ if (getval("auto","")!="") {$error=str_replace("30",$session_length,$lang["sessi
 # Display a no-cookies message
 if (getval("nocookies","")!="" && getval("cookiecheck","")=="") {$error=$lang["nocookies"];}
 
-if (!hook("replaceauth")) {
-# First check that this IP address has not been locked out due to excessive attempts.
-$ip=get_ip();
-$lockouts=sql_value("select count(*) value from ip_lockout where ip='" . escape_check($ip) . "' and tries>='" . $max_login_attempts_per_ip . "' and date_add(last_try,interval " . $max_login_attempts_wait_minutes . " minute)>now()",0);
-
-# Also check that the username provided has not been locked out due to excessive login attempts.
-$ulockouts=sql_value("select count(*) value from user where username='" . getvalescaped("username","") . "' and login_tries>='" . $max_login_attempts_per_username . "' and date_add(login_last_try,interval " . $max_login_attempts_wait_minutes . " minute)>now()",0);
-
-if ($lockouts>0 || $ulockouts>0)
+if (!hook("replaceauth")) 
 	{
-	$error=str_replace("?",$max_login_attempts_wait_minutes,$lang["max_login_attempts_exceeded"]);
-	}
+	# First check that this IP address has not been locked out due to excessive attempts.
+	$ip=get_ip();
+	$lockouts=sql_value("select count(*) value from ip_lockout where ip='" . escape_check($ip) . "' and tries>='" . $max_login_attempts_per_ip . "' and date_add(last_try,interval " . $max_login_attempts_wait_minutes . " minute)>now()",0);
 
-# Process the submitted login
-elseif (array_key_exists("username",$_POST) && getval("langupdate","")=="")
-    {
-    $username=trim(getvalescaped("username",""));
-    $password=trim(getvalescaped("password",""));
+	# Also check that the username provided has not been locked out due to excessive login attempts.
+	$ulockouts=sql_value("select count(*) value from user where username='" . getvalescaped("username","") . "' and login_tries>='" . $max_login_attempts_per_username . "' and date_add(login_last_try,interval " . $max_login_attempts_wait_minutes . " minute)>now()",0);
 
-	$result=perform_login();
-	if ($result['valid'])
+	if ($lockouts>0 || $ulockouts>0)
 		{
-	 	$expires=0;
-       	if (getval("remember","")!="") {$expires = 100;} # remember login for 100 days
+		$error=str_replace("?",$max_login_attempts_wait_minutes,$lang["max_login_attempts_exceeded"]);
+		}
+	# Process the submitted login
+	elseif (array_key_exists("username",$_POST) && getval("langupdate","")=="")
+	    {
+	    $username=trim(getvalescaped("username",""));
+	    $password=trim(getvalescaped("password",""));
 
-		# Store language cookie
-        rs_setcookie("language", $language, 1000); # Only used if not global cookies
-        rs_setcookie("language", $language, 1000, $baseurl_short . "pages/");
+		$result=perform_login();
+		if ($result['valid'])
+			{
+		 	$expires=0;
+	       	if (getval("remember","")!="") {$expires = 100;} # remember login for 100 days
 
-		# Set the session cookie.
-        rs_setcookie("user", "", 0);
+			# Store language cookie
+	        rs_setcookie("language", $language, 1000); # Only used if not global cookies
+	        rs_setcookie("language", $language, 1000, $baseurl_short . "pages/");
 
-		# Set user cookie, setting secure only flag if a HTTPS site, and also setting the HTTPOnly flag so this cookie cannot be probed by scripts (mitigating potential XSS vuln.)
-        rs_setcookie("user", $result['session_hash'], $expires, "", "", substr($baseurl,0,5)=="https", true);
+			# Set the session cookie.
+	        rs_setcookie("user", "", 0);
 
-        # Set default resource types
-        setcookie("restypes",$default_res_types, 0, '', '', false, true);
+			# Set user cookie, setting secure only flag if a HTTPS site, and also setting the HTTPOnly flag so this cookie cannot be probed by scripts (mitigating potential XSS vuln.)
+	        rs_setcookie("user", $result['session_hash'], $expires, "", "", substr($baseurl,0,5)=="https", true);
 
-		# If the redirect URL is the collection frame, do not redirect to this as this will cause
-		# the collection frame to appear full screen.
-		if (strpos($url,"pages/collections.php")!==false) {$url="index.php";}
+	        # Set default resource types
+	        setcookie("restypes",$default_res_types, 0, '', '', false, true);
 
-        $accepted=sql_value("select accepted_terms value from user where username='$username' and (password='$password' or password='".$result['password_hash']."')",0);
-		if (($accepted==0) && ($terms_login) && !checkperm("p")) {redirect ("pages/terms.php?noredir=true&url=" . urlencode("pages/user_preferences.php"));} else {redirect($url);}
-        }
-    else
-        {
-        sleep($password_brute_force_delay);
-        
-		$error=$result['error'];
-        hook("dispcreateacct");
-        }
-    }
-}
+			# If the redirect URL is the collection frame, do not redirect to this as this will cause
+			# the collection frame to appear full screen.
+			if (strpos($url,"pages/collections.php")!==false) {$url="index.php";}
+
+	        $accepted=sql_value("select accepted_terms value from user where username='$username' and (password='$password' or password='".$result['password_hash']."')",0);
+			if (($accepted==0) && ($terms_login) && !checkperm("p")) {redirect ("pages/terms.php?noredir=true&url=" . urlencode("pages/user_preferences.php"));} else {redirect($url);}
+	        }
+	    else
+	        {
+	        sleep($password_brute_force_delay);
+	        
+			$error=$result['error'];
+	        hook("dispcreateacct");
+	        }
+	    }
+	}
 
 if ((getval("logout","")!="") && array_key_exists("user",$_COOKIE))
     {
@@ -105,8 +105,7 @@ if ((getval("logout","")!="") && array_key_exists("user",$_COOKIE))
     	}
     }
 
-    
-    
+
 if (getval("langupdate","")!="")
 	{
 	# Update language while remaining on this page.
@@ -116,11 +115,9 @@ if (getval("langupdate","")!="")
 	}
 
 
-
-
 include "include/header.php";
-if (!hook("replaceloginform")) {
-?>
+if (!hook("replaceloginform")) 
+	{?>
 
   <h1><?php echo text("welcomelogin")?></h1>
   <p><?php echo text(getvalescaped("text","defaultintro"))?></p>
@@ -147,7 +144,7 @@ if (!hook("replaceloginform")) {
 			 <div id="capswarning"><?php echo $lang["caps-lock-on"]; ?></div>
 			<div class="clearerleft"> </div>
 		</div>
-<?php if ($disable_languages==false) { ?>	
+  <?php if ($disable_languages==false) { ?>	
 		<div class="Question">
 			<label for="language"><?php echo $lang["language"]?> </label>
 			<select id="language" class="stdwidth" name="language" onChange="document.getElementById('langupdate').value='YES';document.getElementById('loginform').submit();">
@@ -157,23 +154,21 @@ if (!hook("replaceloginform")) {
 			</select>
 			<div class="clearerleft"> </div>
 		</div> 
-<?php } ?>
+  <?php } ?>
 	
-		<?php if ($allow_keep_logged_in) { ?>
+  <?php if ($allow_keep_logged_in) { ?>
 		<div class="Question">
 			<label for="remember"><?php echo $lang["keepmeloggedin"]?></label>
 			<input style="margin-top: 0.5em;" name="remember" id="remember" type="checkbox" value="yes" <?php echo ($remember_me_checked === true) ? "checked='checked'" : "";?>>
 			<div class="clearerleft"> </div>
 		</div>
-		<?php } ?>
-		
+  <?php } ?>	
 		<div class="QuestionSubmit">
 			<label for="buttons"> </label>			
 			<input name="Submit" type="submit" value="&nbsp;&nbsp;<?php echo $lang["login"]?>&nbsp;&nbsp;" />
 		</div>
 	</form>
   <p>&nbsp;</p>
-
 <?php
 # Javascript to default the focus to the username box
 ?>
