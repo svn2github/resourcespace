d32d368c99daac82e4ecd82388ccf4d46923d152
astec <astec@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2014-07-22 17:55:11 +0000

    FIX poor display of text with ckeditor on edit.php
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@5655 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/edit_fields/8.php
+++ pages/edit_fields/8.php
@@ -4,7 +4,10 @@
 <?php hook("befckeditortextarea"); ?>
 
 <textarea class="stdwidth" rows=20 cols=80 name="<?php echo $name?>" id="<?php echo $name?>" <?php echo $help_js; ?>
-><?php echo htmlspecialchars($value)?></textarea><?php
+><?php if($value == strip_tags($value)){
+	$value=nl2br($value);
+}
+echo htmlspecialchars($value)?></textarea><?php
 
 switch (strtolower($language))
     {
