4aa698a074e0cff723e2400322c1f016655f2745
tgleason <tgleason@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2014-07-23 15:38:21 +0000

    Commented out hook replace_checkbox_vertical_rendering as it causes a reset() error on edit.php
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@5660 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/edit_fields/2.php
+++ pages/edit_fields/2.php
@@ -67,13 +67,13 @@ if ($checkbox_ordered_vertically)
 				$name=$field["ref"] . "_" . md5($option);
 				if ($option!="")
 					{
-					if(!hook("replace_checkbox_vertical_rendering","",array($name,$option,$ref=$field["ref"],$set))){
+					/*if(!hook("replace_checkbox_vertical_rendering","",array($name,$option,$ref=$field["ref"],$set))){*/
 						?>
 						<td width="1"><input type="checkbox" id="<?php echo $name; ?>" name="<?php echo $name?>" value="yes" <?php if (in_array($option,$set)) {?>checked<?php } ?> 
 						<?php if ($edit_autosave) {?>onChange="AutoSave('<?php echo $field["ref"] ?>');" onmousedown="checkbox_allow_save();"<?php } ?>
 						/></td><td><label class="customFieldLabel" for="<?php echo $name; ?>" <?php if($edit_autosave) { ?>onmousedown="checkbox_allow_save();" <?php } ?>><?php echo htmlspecialchars($trans)?></label></td>
 						<?php
-						} # end hook("replace_checkbox_vertical_rendering")
+						/*} # end hook("replace_checkbox_vertical_rendering")*/
 					}
 				}
 			}
