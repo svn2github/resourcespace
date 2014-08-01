b30f13a0f8921b5e7d479bc162b4d38f1c8f0c65
astec <astec@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2014-08-01 14:28:42 +0000

    ADD note to config about additional setting needed to auto rotate images when using staticsync_ingest=false
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@5689 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/config.default.php
+++ include/config.default.php
@@ -1624,7 +1624,7 @@ $staticsync_title_includes_path=true;
 # Should the sync'd resource files be 'ingested' i.e. moved into ResourceSpace's own filestore structure?
 # In this scenario, the sync'd folder merely acts as an upload mechanism. If path to metadata mapping is used then this allows metadata to be extracted based on the file's location.
 $staticsync_ingest=false;
-# Try to rotate images automatically when not ingesting resources?
+# Try to rotate images automatically when not ingesting resources? If set to TRUE you must also set $imagemagick_preserve_profiles=true;
 $autorotate_no_ingest=false;
 #
 # StaticSync Path to metadata mapping
