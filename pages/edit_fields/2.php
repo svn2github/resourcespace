52c6511c0e4c7fab1fa2eb6ce3f3ee2067542240
mhinton <mhinton@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2014-08-18 11:04:24 +0000

    FIX - Revert changes to checkbox layout (causing incorrect rendering)
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@5744 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/edit_fields/2.php
+++ pages/edit_fields/2.php
@@ -21,6 +21,15 @@ if ($auto_order_checkbox) {natsort($option_trans);}
 $options=array_keys($option_trans); # Set the options array to the keys, so it is now effectively sorted by translated string	
 	
 $set=trim_array(explode(",",$value));
+$wrap=0;
+$l=average_length($option_trans_simple);
+$cols=10;
+if ($l>5)  {$cols=6;}
+if ($l>10) {$cols=4;}
+if ($l>15) {$cols=3;}
+if ($l>25) {$cols=2;}
+
+$height=ceil(count($options)/$cols);
 
 if ($edit_autosave) { ?>
 	<script type="text/javascript">
@@ -38,21 +47,12 @@ if ($edit_autosave) { ?>
 global $checkbox_ordered_vertically;
 if ($checkbox_ordered_vertically)
 	{
-	$wrap=0;
-	$l=average_length($option_trans_simple);
-	$cols=10;
-	if ($l>5)  {$cols=6;}
-	if ($l>10) {$cols=4;}
-	if ($l>15) {$cols=3;}
-	if ($l>25) {$cols=2;}
-
-	$height=ceil(count($options)/$cols);
 	if(!hook('rendereditchkboxes')):
 	# ---------------- Vertical Ordering (only if configured) -----------
 	?>
 	<fieldset class="customFieldset" name="<?php echo $field['title']; ?>">
 		<legend class="accessibility-hidden"><?php echo $field['title']; ?></legend>
-		<div class="verticaleditcheckboxes"><?php
+		<table cellpadding=2 cellspacing=0><tr><?php
 	for ($y=0;$y<$height;$y++)
 		{
 		for ($x=0;$x<$cols;$x++)
@@ -69,35 +69,37 @@ if ($checkbox_ordered_vertically)
 					{
 					/*if(!hook("replace_checkbox_vertical_rendering","",array($name,$option,$ref=$field["ref"],$set))){*/
 						?>
-						<div class="checkoption"><span class="checkbox"><input type="checkbox" id="<?php echo $name; ?>" name="<?php echo $name?>" value="yes" <?php if (in_array($option,$set)) {?>checked<?php } ?> 
+						<td width="1"><input type="checkbox" id="<?php echo $name; ?>" name="<?php echo $name?>" value="yes" <?php if (in_array($option,$set)) {?>checked<?php } ?> 
 						<?php if ($edit_autosave) {?>onChange="AutoSave('<?php echo $field["ref"] ?>');" onmousedown="checkbox_allow_save();"<?php } ?>
-						/></span><span class="checkboxtext"><label class="customFieldLabel" for="<?php echo $name; ?>" <?php if($edit_autosave) { ?>onmousedown="checkbox_allow_save();" <?php } ?>><?php echo htmlspecialchars($trans)?></label></span></div>
+						/></td><td><label class="customFieldLabel" for="<?php echo $name; ?>" <?php if($edit_autosave) { ?>onmousedown="checkbox_allow_save();" <?php } ?>><?php echo htmlspecialchars($trans)?></label></td>
 						<?php
 						/*} # end hook("replace_checkbox_vertical_rendering")*/
 					}
 				}
-			}?><br /><?php
+			}
+		?></tr><tr><?php
 		}
-	?></div></fieldset><?php
+	?></tr></table></fieldset><?php
 	endif;
 	}
 else
 	{				
 	# ---------------- Horizontal Ordering (Standard) ---------------------				
 	?>
-	<div class="editcheckboxes">
+	<table cellpadding=2 cellspacing=0><tr>
 	<?php
 
 	foreach ($option_trans as $option=>$trans)
 		{
 		$name=$field["ref"] . "_" . md5($option);
+		$wrap++;if ($wrap>$cols) {$wrap=1;?></tr><tr><?php }
 		?>
-		<div class="checkoption"><span class="checkbox"><input type="checkbox" name="<?php echo $name?>" value="yes" <?php if (in_array($option,$set)) {?>checked<?php } ?>
+		<td width="1"><input type="checkbox" name="<?php echo $name?>" value="yes" <?php if (in_array($option,$set)) {?>checked<?php } ?>
 		<?php if ($edit_autosave) {?>onChange="AutoSave('<?php echo $field["ref"] ?>');"<?php } ?>
-		 /></span><span class="checkboxtext"><?php echo htmlspecialchars($trans)?>&nbsp;</span></div>
+		 /></td><td><?php echo htmlspecialchars($trans)?>&nbsp;</td>
 		<?php
 		}
-	?></div><?php
+	?></tr></table><?php
 	}
 	
 endif;
