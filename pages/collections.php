fbdf8c14bdd1d2efe8b4ce820322b861725faa93
astec <astec@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2014-08-21 19:24:42 +0000

    CHG Add containing div around external share collection-max stats and added vertical space between stats and tools
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@5773 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/collections.php
+++ pages/collections.php
@@ -484,8 +484,10 @@ elseif ($k!="" && !$lazyload)
 <div id="CollectionMenu">
   <h2><?php echo i18n_get_collection_name($tempcol)?></h2>
 	<br />
+	<div class="CollectionStatsAnon">
 	<?php echo $lang["created"] . " " . nicedate($tempcol["created"])?><br />
   	<?php echo $count_result . " " . $lang["youfoundresources"]?><br />
+  	</div>
     <?php if ((isset($zipcommand) || $collection_download) && $count_result>0) { ?>
 	<a href="<?php echo $baseurl_short?>pages/terms.php?k=<?php echo urlencode($k) ?>&url=<?php echo urlencode("pages/collection_download.php?collection=" .  $usercollection . "&k=" . $k)?>" onclick="return CentralSpaceLoad(this,true);">&gt;&nbsp;<?php echo $lang["action-download"]?></a>
 	<?php }
