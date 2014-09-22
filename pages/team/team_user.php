ebc1839c159d8a480547aefd240d343e427e4bde
jethro <jethro@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2014-09-22 09:35:33 +0000

    FIX pager for user manage in team centre, missed from last commit
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@5879 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/team/team_user.php
+++ pages/team/team_user.php
@@ -76,13 +76,13 @@ $atoz.="</div>";
 
 ?>
 
-<div class="TopInpageNav"><?php echo $atoz?>	<div class="InpageNavLeftBlock"><?php echo $lang["resultsdisplay"]?>:
+<div class="TopInpageNav"><div class="TopInpageNavLeft"><?php echo $atoz?>	<div class="InpageNavLeftBlock"><?php echo $lang["resultsdisplay"]?>:
 	<?php 
 	for($n=0;$n<count($list_display_array);$n++){?>
 	<?php if ($per_page==$list_display_array[$n]){?><span class="Selected"><?php echo $list_display_array[$n]?></span><?php } else { ?><a href="<?php echo $url; ?>&per_page_list=<?php echo $list_display_array[$n]?>" onClick="return CentralSpaceLoad(this);"><?php echo $list_display_array[$n]?></a><?php } ?>&nbsp;|
 	<?php } ?>
 	<?php if ($per_page==99999){?><span class="Selected"><?php echo $lang["all"]?></span><?php } else { ?><a href="<?php echo $url; ?>&per_page_list=99999" onClick="return CentralSpaceLoad(this);"><?php echo $lang["all"]?></a><?php } ?>
-	</div> <?php pager(false); ?></div>
+	</div></div> <?php pager(false); ?></div>
 
 <strong><?php echo $lang["total"] . ": " . count($users); ?> </strong><?php echo $lang["users"]; ?>
 <br />
