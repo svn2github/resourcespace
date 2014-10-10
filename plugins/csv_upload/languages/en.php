0acf70dea5e5b6b8eed12ca98c4c7f44cd6b3ed7
nbraddy <nbraddy@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2014-10-10 07:16:49 +0000

    CHG - Amended CSV upload instructions
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@5925 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/csv_upload/languages/en.php
+++ plugins/csv_upload/languages/en.php
@@ -8,10 +8,10 @@ $lang["csv_upload_nav_link"]="CSV upload";
 $lang["csv_upload_intro"]="<p>This plugin allows you to create resources by uploading a CSV file. The format of the CSV is important and must follow a defined format.</p>";
 
 $lang["csv_upload_condition1"]="<li>The CSV must have a header row</li>";
-$lang["csv_upload_condition2"]="<li>To create resources of different resource types there must be a column named 'resource_type'</li>";
-$lang["csv_upload_condition3"]="<li>To assign different archive states to the resources there must be a column named 'status'</li>";
-$lang["csv_upload_condition4"]="<li>To assign different access levels (open,restricted, confidential) states to the resources there must be a column named 'access'</li>";
-$lang["csv_upload_condition5"]="<li>To be able to upload resource files later using batch replace functionality, each resource there should be a column named 'Original filename' and each file should have a unique filename</li>";
+$lang["csv_upload_condition2"]="<li>To create resources of different resource types there must be a column named 'resource_type' containing the reference ID of the resource type (e.g. 1 is Photo)</li>";
+$lang["csv_upload_condition3"]="<li>To assign different archive states to the resources there must be a column named 'status' containing the status ID." . " -2=" . $lang["status-2"] . ", -1=" . $lang["status-1"] . ", 0=" . $lang["status0"] . ", 1=" . $lang["status1"] . ", 2=" . $lang["status2"] . "</li>";
+$lang["csv_upload_condition4"]="<li>To assign different access levels (open,restricted, confidential) states to the resources there must be a column named 'access' with access values: 0=" . $lang["access0"] . ", 1=" . $lang["access1"] . ", 2=" . $lang["access2"] . "</li>";
+$lang["csv_upload_condition5"]="<li>To be able to upload resource files later using batch replace functionality there should be a column named 'Original filename' and each file should have a unique filename</li>";
 $lang["csv_upload_condition6"]="<li>All other column headers must correspond to the full name of a resource metadata field</li>";
 $lang["csv_upload_condition7"]="<li>All mandatory fields for the created resource types must be present</li>";
 $lang["csv_upload_error_no_permission"]="You do not have the correct permissions to upload a CSV file";
