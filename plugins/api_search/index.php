a5ec3ade63952ecaa3612da03e92d723342ab114
astec <astec@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2014-11-06 18:28:47 +0000

    ADD include collections_functions
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@6011 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/api_search/index.php
+++ plugins/api_search/index.php
@@ -4,6 +4,7 @@ include(dirname(__FILE__)."/../../include/db.php");
 include(dirname(__FILE__)."/../../include/general.php");
 include(dirname(__FILE__)."/../../include/search_functions.php");
 include(dirname(__FILE__)."/../../include/resource_functions.php");
+include(dirname(__FILE__)."/../../include/collections_functions.php");
 $api=true;
 
 include(dirname(__FILE__)."/../../include/authenticate.php");
