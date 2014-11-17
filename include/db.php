18004852b5e2672988ad27c344ffdc13864ee87d
danh <danh@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2014-11-17 13:02:16 +0000

    CHG: Updated the look of the error message box to better match the current design
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@6035 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/db.php
+++ include/db.php
@@ -38,11 +38,11 @@ function errorhandler($errno, $errstr, $errfile, $errline)
 	if (!error_reporting()) {return true;}
 	?>
 	</select></table></table></table>
-	<div style="border:1px solid black;font-family:verdana,arial,helvetica;position:absolute;top:100px;left:100px; background-color:white;width:400px;padding:20px;border-bottom-width:4px;border-right-width:4px;font-size:15px;color:black;">
-	<table cellpadding=5 cellspacing=0><tr><td valign=middle><img src="<?php echo $baseurl?>/pages/admin/gfx/cherrybomb.gif" width="48" height="48"></td><td valign=middle align=left><span style="font-size:22px;">Sorry, an error has occurred.</span></td></tr></table>
-	<p style="font-size:11px;color:black;margin-top:20px;">Please <a href="#" onClick="history.go(-1)">go back</a> and try something else.</p>
+	<div style="box-shadow: 3px 3px 20px #666;font-family:ubuntu,arial,helvetica,sans-serif;position:absolute;top:150px;left:150px; background-color:white;width:450px;padding:20px;font-size:15px;color:#fff;border-radius:5px;">
+	<table cellpadding=5 cellspacing=0><tr><td valign=middle><img src="<?php echo $baseurl?>/pages/admin/gfx/cherrybomb.gif" width="48" height="48"></td><td valign=middle align=left><span style="font-size:26px;">Sorry, an error has occurred</span></td></tr></table>
+	<p style="font-size:14px;color:black;margin-top:20px;">Please <a href="#" onClick="history.go(-1)">go back</a> and try something else.</p>
 	<?php global $show_error_messages; if ($show_error_messages) { ?>
-	<p style="font-size:11px;color:black;">You can <a href="<?php echo $baseurl?>/pages/check.php">check</a> your installation configuration.</p>
+	<p style="font-size:14px;color:black;">You can <a href="<?php echo $baseurl?>/pages/check.php">check</a> your installation configuration.</p>
 	<hr style="margin-top:20px;"><p style="font-size:11px;color:black;"><?php echo htmlspecialchars("$errfile line $errline: $errstr"); ?></p>
 	<?php } ?>
 	</div>
