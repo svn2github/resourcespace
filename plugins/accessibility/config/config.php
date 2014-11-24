c51334bb525b5d0f0bc1fc5c1b2ad0cbe3c7d3d8
acota <acota@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2014-11-24 15:12:50 +0000

    ADD accessibility plugin - High contrast plugin for partially blind users
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@6064 c08608d7-6e46-0410-86ca-f2a6f1370df5

---
+++ plugins/accessibility/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+#$userfixedtheme="accessibility";
+global $available_themes;
+$available_themes[]="accessibility";
+
+$header_favicon="plugins/greyspace/gfx/interface/favicon.png";
+
+$iconthumbs=false;
