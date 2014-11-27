05be0a859abc3052e160abdbd39ee2a98b4aa360
acota <acota@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2014-11-27 10:30:06 +0000

    FIX wrong variable name
    git-svn-id: http://svn.resourcespace.org/svn/resourcespace@6076 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/track_field_history/hooks/view.php
+++ plugins/track_field_history/hooks/view.php
@@ -5,13 +5,13 @@
 function HookTrack_field_historyViewDisplay_field_modified_value($field)
 {
 
-	global $ref, $track_fields, $baseurl_short;
+	global $ref, $track_fields, $baseurl;
 
 	if(in_array($field['ref'], $track_fields)) {
 
 		$get_params = '?ref=' . $ref . '&field=' . $field['ref'] . '&field_title=' . $field['title'];
 
-		$field['value'] .= '<a href="' . $baseurl_short . '/plugins/track_field_history/pages/field_history_log.php' . $get_params . '" style="margin-left: 20px;">&gt;&nbsp;History</a>';
+		$field['value'] .= '<a href="' . $baseurl . '/plugins/track_field_history/pages/field_history_log.php' . $get_params . '" style="margin-left: 20px;">&gt;&nbsp;History</a>';
 
 	}
 
